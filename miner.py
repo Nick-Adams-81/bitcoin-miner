5d10139e1ca7618b9d1276dc826daa1abc3ec4f3
nick adams <thebigdeal916@icloud.com>
2021-02-07 14:49:04 -0600

    mine base code

--- miner.py
+++ miner.py
@@ -1,1 +1,31 @@
-print("hello!!!")
+from hashlib import sha256
+MAX_NONCE = 10000000
+
+def SHA256(text):
+    return sha256("abc".encode("ascii")).hexdigest()
+
+def mine(block_number, transactions, previous_hash, prefix_zeros):
+    prefix_str = '0'* prefix_zeros
+    for nonce in range(MAX_NONCE):
+        text = str(block_number) + transactions + previous_hash + str(nonce)
+        new_hash = SHA256(text)
+        if new_hash.startswith(prefix_str):
+            print("Successfully mined bitcoins with nonce value:{nonce}")
+            return new_hash
+
+    raise BaseException(f"Cant find correct has after trying {MAX_NONCE} times")
+
+if __name__=='__main__':
+    transactions ='''
+    nick -> walt -> 20,
+    ernie -> jenna -> 45
+    '''
+    dificulty=1
+    import time
+    start = time.time()
+    print("Start mining!")
+    new_hash = mine(5,transactions,'ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad',dificulty)
+    total_time = str((time.time() - start))
+    print(f"Done mining. Mining took: {total_time} seconds")
+
+    print(new_hash)
