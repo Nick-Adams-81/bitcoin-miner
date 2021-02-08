52290ef2c5eb3eb731f4051d2592b6844cb612be
nick adams <thebigdeal916@icloud.com>
2021-02-08 17:16:33 -0600

    added notes

--- miner.py
+++ miner.py
@@ -1,9 +1,10 @@
 from hashlib import sha256
+# MAX_NONCE variable is number of times the loop runs to find the correct blockchain #
 MAX_NONCE = 10000000
-
+# SHA256 function is to encode the blockchain to read it in the terminal #
 def SHA256(text):
     return sha256(text.encode("ascii")).hexdigest()
-
+# mine function is to search for blockchain #
 def mine(block_number, transactions, previous_hash, prefix_zeros):
     prefix_str = '0' * prefix_zeros
     for nonce in range(MAX_NONCE):
@@ -12,7 +13,7 @@ def mine(block_number, transactions, previous_hash, prefix_zeros):
         if new_hash.startswith(prefix_str):
             print(f"Successfully mined bitcoins with nonce value of: {nonce}")
             return new_hash
-   
+   # raising an exception so the loop doesnt run infinite number of times #
     raise BaseException(f"Cant find correct after trying {MAX_NONCE} times")
 
 if __name__ == '__main__':
@@ -20,7 +21,9 @@ if __name__ == '__main__':
     nick -> walt -> 20,
     jenna -> ernie -> 45
     '''
-    dificulty = 5
+    # dificulty variable is umber of zeros in blockchain #
+    dificulty = 6
+    # setting a timer to see how long it takes to find blockchains #
     import time
     start = time.time()
     print("Start mining!")
